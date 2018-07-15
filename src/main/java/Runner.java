36b28c70c7687896639a020711db051c4353e365
ZykovAleksandr <umalog@icloud.com>
2018-07-15 22:54:55 +0300

    переключен основной сервис

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -4,15 +4,15 @@ import helper.IoHelper;
 public class Runner {
 
     /**
-     * При большом размере входящего файла (размером от "Война и мир" и более),
-     * переключить интерфейс FileParser с SimpleFileParser на ParallelFileParser.
+     * При малом размере входящего файла (размером менее "Война и мир"),
+     * переключить интерфейс FileParser с ParallelFileParser на SimpleFileParser.
      *
      * @param args массив из 3-х строк:
      *             путь до считываемого файла, выходной файл(дата-сумма), выходной файл(офис-сумма).
      */
     public static void main(String[] args) {
         if (IoHelper.validateArgs(args)) {
-            FileParser parser = new SimpleFileParser();
+            FileParser parser = new ParallelFileParser();
             parser.parse(args[0], args[1], args[2]);
         } else {
             throw new IllegalArgumentException("Неверное количество передаваемых параметров. \n" +
